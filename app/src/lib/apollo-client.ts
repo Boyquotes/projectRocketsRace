e9e94a92336142e20b4d058cf6f84287095e6912
Boyquotes <webmaster@linux-live-cd.org>
2025-01-01 14:43:43 +0100

    Fix ws client for subscriptions

--- app/src/lib/apollo-client.ts
+++ app/src/lib/apollo-client.ts
@@ -1,10 +1,30 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink, split } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import {GraphQLWsLink} from "@apollo/client/link/subscriptions"
+import { createClient } from 'graphql-ws';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql', // Adjust this to your GraphQL server URL
+  uri: 'http://localhost:4000/graphql', // HTTP endpoint for queries and mutations
 });
 
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'ws://localhost:4000/graphql', // WebSocket endpoint for subscriptions
+}));
+
+// Split link based on operation type
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
 export const client = new ApolloClient({
-  link: httpLink,
+  link: splitLink,
   cache: new InMemoryCache(),
 });
